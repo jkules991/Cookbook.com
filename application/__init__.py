827563d7e9fb2fb9fe934e18ad6f2bc2809d149f
jkules991 <kulesjosip@hotmail.com>
2022-11-14 10:05:42 +0000

    working connects to database

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 from application import routes
