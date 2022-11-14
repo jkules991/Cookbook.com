712b7efbf5f97da1a6f7718d76e6b1c4325287ac
jkules991 <kulesjosip@hotmail.com>
2022-11-14 21:41:15 +0000

    Create finished for real this time num 2

--- application/forms.py
+++ application/forms.py
@@ -4,8 +4,8 @@ from wtforms import StringField, SubmitField, IntegerField
 class RnameForm(FlaskForm):
     name_form = StringField("Recipe Name:")
     ingredients_form = StringField("Ingredients:")
-    prep_t_form = IntegerField("How long to prepare:")
-    cook_t_form = IntegerField("How long to cook:")
+    prep_t_form = IntegerField("How long to prepare (minutes):")
+    cook_t_form = IntegerField("How long to cook (minutes):")
     submit = SubmitField("Submit")
 
 class RprepForm(FlaskForm):
